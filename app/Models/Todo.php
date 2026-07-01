83975fa91b90b408e16cfc52ee9f7add1ae7d052
railey <raileyguinto2@gmail.com>
2026-07-01 15:30:37 +0800

    feat: add Spatie activity logs for todos

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
 class Todo extends Model
 {
+    use LogsActivity;
+
     protected $fillable = [
         'title',
         'description',
@@ -19,5 +23,20 @@ class Todo extends Model
         return [
             'deadline' => 'date',
         ];
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('todo')
+            ->logOnly([
+                'title',
+                'description',
+                'priority',
+                'deadline',
+                'status',
+            ])
+            ->logOnlyDirty()
+            ->dontLogEmptyChanges();
     }
 }
